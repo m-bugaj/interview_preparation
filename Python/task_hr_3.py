31f94c37022c2be5a202f19f52c179986225cac4
bugaj55113 <bbugi2016@gmail.com>
2024-02-27 21:11:44 +0100

    HackerRank - Task 3

---
+++ Python/task_hr_3.py
@@ -0,0 +1,12 @@
+# TASK:
+# You are given three integers: , , and . Print two lines.
+# On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+
+# SOLUTION:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(pow(a,b,))
+    print(pow(a,b,m))
