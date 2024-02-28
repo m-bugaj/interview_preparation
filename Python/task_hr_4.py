d70005d5392483ea1f8e79358f295bf843b9776e
bugaj55113 <bbugi2016@gmail.com>
2024-02-28 18:52:11 +0100

    HackerRank - Task 4

---
+++ Python/task_hr_4.py
@@ -0,0 +1,13 @@
+# TASK:
+
+# Read four numbers a,b,c,d and print the result of a^b+c^d.
+
+# SOLUTION:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    
+    print(pow(a,b) + pow(c,d))
