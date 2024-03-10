7d692f6d0244aa97de0d67596f6df419ca28782e
bugaj55113 <bbugi2016@gmail.com>
2024-03-10 13:54:51 +0100

    HackerRank - Interview Preparation - 2D Array - DS

---
+++ The HackerRank Interview Preparation Kit/hr_2D_Array_DS.py
@@ -0,0 +1,68 @@
+# TASK:
+# Given a 6x6 2D Array, arr:
+
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# An hourglass in A is a subset of values with indices falling in this pattern in arr's graphical representation:
+
+# a b c
+#   d
+# e f g
+# There are 16 hourglasses in arr. An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum. The array will always be 6x6.
+
+# Sample Input
+
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 2 4 4 0
+# 0 0 0 2 0 0
+# 0 0 1 2 4 0
+# Sample Output
+
+# 19
+
+# SOLUTION:
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    results = []
+    for i in range(len(arr) - 2):
+        for j in range(len(arr[i]) - 2):
+            result = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            results.append(result)
+            
+    return max(results)
+
+if __name__ == '__main__':
+    fptr = open('output/output_2d_arrar_ds.txt', 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
