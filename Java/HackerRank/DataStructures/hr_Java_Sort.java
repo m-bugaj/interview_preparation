a6433bb2f141b8cc8a1a08ff8a66019f0654f29f
bugaj55113 <bbugi2016@gmail.com>
2024-04-19 11:02:22 +0200

    HackerRank - Java - Data Structures - Java Sort

---
+++ Java/HackerRank/DataStructures/hr_Java_Sort.java
@@ -0,0 +1,108 @@
+// TASK:
+
+// You are given a list of student information: ID, FirstName, and CGPA. Your task is to rearrange them according to their CGPA in decreasing order. If two student have the same CGPA, then arrange them according to their first name in alphabetical order. If those two students also have the same first name, then order them according to their ID. No two students have the same ID.
+
+// Hint: You can use comparators to sort a list of objects. See the oracle docs to learn about comparators.
+
+// Input Format
+
+// The first line of input contains an integer N, representing the total number of students. The next N lines contains a list of student information in the following structure:
+
+// ID Name CGPA
+
+// The name contains only lowercase English letters. The  contains only integer numbers without leading zeros. The CGPA will contain, at most, 2 digits after the decimal point.
+
+// Output Format
+
+// After rearranging the students according to the above rules, print the first name of each student on a separate line.
+
+// Sample Input
+
+// 5
+// 33 Rumpa 3.68
+// 85 Ashis 3.85
+// 56 Samiha 3.75
+// 19 Samara 3.75
+// 22 Fahim 3.76
+// Sample Output
+
+// Ashis
+// Fahim
+// Samara
+// Samiha
+// Rumpa
+
+// SOLUTION:
+
+package Java.HackerRank.DataStructures;
+
+import java.util.*;
+
+class Checker implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        
+        if (s1.getCgpa() > s2.getCgpa()){
+            return -1;
+        } else if (s1.getCgpa() < s2.getCgpa()) {
+            return 1;
+        } else {
+            if (!s1.getFname().equals(s2.getFname())) {
+                return s1.getFname().compareTo(s2.getFname());
+            } else {
+                return s1.getId() - s2.getId();
+            }
+        }
+        
+    }
+}
+
+class Student{
+	private int id;
+	private String fname;
+	private double cgpa;
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+}
+
+//Complete the code
+
+public class hr_Java_Sort {
+    public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+        Checker checker = new Checker();
+		
+		List<Student> studentList = new ArrayList<Student>();
+		while(testCases>0){
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+			
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+			
+			testCases--;
+		}
+        
+        in.close();
+        
+        Collections.sort(studentList, checker);
+      
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+	}
+}
