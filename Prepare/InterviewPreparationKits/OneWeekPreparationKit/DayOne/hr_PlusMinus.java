64716d15dd035c7a98da4b00f341e691c760bbf2
bugaj55113 <bbugi2016@gmail.com>
2024-05-18 16:30:04 +0200

    Prepare - Interview Preparation Kits - Day 1 - Plus Minus

---
+++ Prepare/InterviewPreparationKits/OneWeekPreparationKit/DayOne/hr_PlusMinus.java
@@ -0,0 +1,86 @@
+// TASK:
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+// Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.
+
+// Example
+
+// There are n=5 elements, two positive, two negative and one zero. Their ratios are 2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000. Results are printed as:
+
+// 0.400000
+// 0.400000
+// 0.200000
+
+// Sample Input
+
+// STDIN           Function
+// -----           --------
+// 6               arr[] size n = 6
+// -4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+// Sample Output
+
+// 0.500000
+// 0.333333
+// 0.166667
+
+// SOLUTION:
+
+package Prepare.InterviewPreparationKits.OneWeekPreparationKit.DayOne;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+        Integer positive = 0;
+        Integer negative = 0;
+        Integer zero = 0;
+        for (int i=0; i<arr.size(); i++) {
+            if (arr.get(i) > 0) {
+                positive++;
+            } else if (arr.get(i) < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+        
+        System.out.printf("%.6f\n", (float) positive/arr.size());
+        System.out.printf("%.6f\n", (float) negative/arr.size());
+        System.out.printf("%.6f\n", (float) zero/arr.size());
+    }
+
+}
+
+public class hr_PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
